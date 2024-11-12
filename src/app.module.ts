871bb79ad42ff6276bf4c456afcc66f9a6eb96f5
thesunnatillo <sunnatillocoder@gmail.com>
2024-11-12 20:29:53 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { TasksService } from './modules/tasks/tasks.service';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
