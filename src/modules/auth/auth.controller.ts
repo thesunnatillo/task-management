acfbc18428fd164b37aa562570cf177b2e7c9cfb
thesunnatillo <sunnatillocoder@gmail.com>
2024-11-13 12:59:49 +0500

    signup va login qilindi

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { LogInDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly 
-  ) {}
+  constructor(private readonly authService: AuthService) {}
   @Post('signup')
-  signup() {
+  signup(@Body() signUpDto: SignupDto) {
+    return this.authService.signup(signUpDto);
+  }
 
+  @Post('login')
+  login(@Body() logInDto: LogInDto) {
+    return this.authService.login(logInDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './entities/auth.entity';
+import { Repository } from 'typeorm';
+import { SignupDto } from './dto/signup.dto';
+import { LoginAlreadyUsed, UsernameOrPasswordWrong } from './exception/auth.exception';
+import { LogInI, SignUpI } from './interfaces/auth.i';
+import { LogInDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(Users)
+    private readonly usersRepo: Repository<Users>,
+  ) {}
+
+  async signup(signUpDto: SignupDto): Promise<SignUpI> {
+    const user = await this.usersRepo.findOne({
+      where: { username: signUpDto.username },
+    });
+
+    if (user) {
+      throw new LoginAlreadyUsed();
+    }
+    const encoded = Buffer.from(
+      `${signUpDto.username}:${signUpDto.password}`,
+    ).toString('base64');
+    const newUser = {
+      ...signUpDto,
+      hash: encoded,
+    };
+
+    const savedUser = await this.usersRepo.save(newUser);
+
+    return {
+      encoded: savedUser.hash,
+    };
+  }
+
+  async login(logInDto: LogInDto): Promise<LogInI> {
+    const user = await this.usersRepo.findOne({
+      where: { username: logInDto.username },
+    });
+    if (!user) {
+      throw new UsernameOrPasswordWrong();
+    }
+    const decode = Buffer.from(user.hash, 'base64').toString('ascii');
+    const password = decode.split(':')[1];
+
+    if (password !== logInDto.password) {
+      throw new UsernameOrPasswordWrong();
+    }
+
+    return {
+      message: 'Done!',
+    };
+  }
+}
