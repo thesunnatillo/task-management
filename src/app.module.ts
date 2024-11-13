5079b339f0ed67bc9be7640cd100aabd43a167e0
thesunnatillo <sunnatillocoder@gmail.com>
2024-11-13 12:58:37 +0500

    psql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { TasksService } from './modules/tasks/tasks.service';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './modules/auth/entities/auth.entity';
+import { Tasks } from './modules/tasks/entities/task.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [AuthModule, TasksModule],
+  imports: [
+    AuthModule,
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [Users, Tasks],
+    }),
+  ],
   controllers: [],
   providers: [TasksService],
 })
