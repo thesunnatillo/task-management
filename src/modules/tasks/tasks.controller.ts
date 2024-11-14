30924e39dce42048a666e0108f96c7fe497a2e6e
thesunnatillo <sunnatillocoder@gmail.com>
2024-11-14 16:35:00 +0500

    udate

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create() {}
+
+  @Get()
+
+}
