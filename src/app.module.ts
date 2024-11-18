767c77c6bc5958f9285eabf86ac418df96687ad9
thesunnatillo <sunnatillocoder@gmail.com>
2024-11-18 20:58:05 +0500

    edit typeormModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './modules/auth/entities/auth.entity';
 import { Tasks } from './modules/tasks/entities/task.entity';
 import * as dotenv from 'dotenv';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envValidation } from './env.validation';
 dotenv.config();
+
+const configService = new ConfigService();
 
 @Module({
   imports: [
     AuthModule,
     TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidation,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      host: configService.get<string>('POSTGRES_HOST'),
+      port: configService.get<number>('POSTGRES_PORT'),
+      username: configService.get<string>('POSTGRES_USER'),
+      password: configService.get<string>('POSTGRES_PASSWORD'),
+      database: configService.get<string>('POSTGRES_DATABASE'),
       entities: [Users, Tasks],
     }),
   ],
